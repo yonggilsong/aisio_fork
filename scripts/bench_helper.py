xnvme/aisio

feat(bench/compare): add fio xnvme compare workflow
Add a dedicated fio+xNVMe compare workflow for SPDK and uPCIe.

Run read, randread, write, and randwrite in one workflow. Store
raw results under a BDF-specific artifact root, collect them into
a single benchmark-results.json, and render a compare-specific
HTML report with the benchmark-fio-compare template.

Signed-off-by: Yonggil Song <yonggil.song@samsung.com>

scripts/bench_helper.py
@@ -32,8 +32,14 @@ import logging as log
 
 from bdevperf import bdevperf_cmd, create_config as bdevperf_config
 from dcgm_helper import DcgmHelper
+from fio_xnvme import create_fio_job, fio_xnvme_cmd
 from spdk_nvme_perf import spdk_nvme_perf_cmd
 from xnvmeperf import xnvmeperf_cmd, xnvmeperf_cuda_cmd
+
+
+FIO_TIME_BASED_OFF = 0
+FIO_PREFILL_BS = 131072
+FIO_PREFILL_QD = 32
 
 
 class BenchHelper():
@@ -45,6 +51,7 @@ class BenchHelper():
             cfm: CpuFrequencyHelper,
             tool: str = "bdevperf",
             backend: str = "spdk",
+            fio_size: str = "16GiB",
     ):
         self.initialised = False
 
@@ -55,6 +62,8 @@ class BenchHelper():
         self.stress = False
         self.backend = backend
         self.tool = tool
+        self.fio_size = fio_size
+        self.fio_prefilled = set()
 
         self.dcgm = DcgmHelper(cijoe) if backend == "upcie-cuda" else None
 
@@ -79,8 +88,11 @@ class BenchHelper():
                 self.bin = Path(spdk_path)  / "build" / "bin" / "spdk_nvme_perf"
         elif tool in ["xnvmeperf", "xnvmeperf-cuda"]:
             self.bin = "xnvmeperf"
+        elif tool == "fio_xnvme":
+            self.bin = "fio"
         else:
             log.error(f"Failed: Unknown tool({tool})")
+            return
 
         self.initialised = True
 
@@ -98,10 +110,9 @@ class BenchHelper():
             return err
 
         self.use_thrsib = use_thrsib
-
         return 0
 
-    def run_benchmark(self, depth: int, size: int, ndevs: int, ncpus: int, time: int, cpu_freq: float, suffix: str = "", nqueues: int = 1):
+    def run_benchmark(self, rw: str, depth: int, size: int, ndevs: int, ncpus: int, time: int, cpu_freq: float, suffix: str = "", nqueues: int = 1):
         if not self.initialised:
             log.error("Failed: benchmarker not initialised correctly")
             return 1, None
@@ -116,8 +127,8 @@ class BenchHelper():
             )
         else:
             filename = (
-                f"d{ndevs}-c{ncpus}-o{size}-q{depth}-"
-                f"be_{self.backend}-tool_{self.tool}-"
+                f"d{ndevs}-c{ncpus}-o{size}-f_{self.fio_size}-"
+                f"rw_{rw}-q{depth}-be_{self.backend}-tool_{self.tool}-"
                 f"thrsib{1 if self.use_thrsib else 0}-"
                 f"freq_{cpu_freq}-"
                 f"stress{1 if self.stress else 0}"
@@ -131,7 +142,7 @@ class BenchHelper():
                 return 0, result
 
         bench_args = {
-            "iopattern": "randread",
+            "iopattern": rw,
             "qdepth": depth,
             "iosize": size,
             "runtime": time,
@@ -141,7 +152,23 @@ class BenchHelper():
         if not is_cuda:
             bench_args["cpumask"] = self.cpu_masks[ncpus]
 
-        command = f"/usr/bin/time "
+        if self.tool == "fio_xnvme" and ndevs != 1:
+            log.error("Failed: fio_xnvme currently supports exactly 1 device per benchmark point")
+            return 1, None
+
+        if is_cuda:
+            selected_cpus = []
+            command = f"/usr/bin/time "
+        else:
+            selected_cpus = [v[0] for v in self.cpu_pairs if int(bench_args["cpumask"], 16) & (1 << v[0])]
+            if self.tool == "fio_xnvme" and rw in ["read", "randread"]:
+                err = self._prepare_fio_read_working_set(self.devices[0]["pci_addr"], rw, self.backend)
+                if err:
+                    log.error("Failed: _prepare_fio_read_working_set()")
+                    return err, None
+
+            cpu_list = ",".join(str(cpu) for cpu in selected_cpus)
+            command = f"taskset -c {cpu_list} /usr/bin/time "
 
         if self.tool == "bdevperf":
             config_local_path = self.configs_path / f"d{ndevs}.json"
@@ -150,27 +177,23 @@ class BenchHelper():
 
             bench_args["config_path"] = self.remote_config
             command += bdevperf_cmd(self.bin, bench_args)
-
         elif self.tool == "spdk_nvme_perf":
             command += spdk_nvme_perf_cmd(self.bin, bench_args)
-
         elif self.tool == "xnvmeperf":
             bench_args["backend"] = self.backend
             command += xnvmeperf_cmd(self.bin, bench_args)
-
         elif is_cuda:
             bench_args["backend"] = self.backend
             bench_args["nqueues"] = nqueues
             command += xnvmeperf_cuda_cmd(self.bin, bench_args)
-
+        elif self.tool == "fio_xnvme":
+            fio_job = create_fio_job(
+                self.devices[0]["pci_addr"], size, depth, time, rw, self.backend, self.fio_size
+            )
+            command += fio_xnvme_cmd(self.bin, fio_job)
         else:
             log.error(f"Unknown tool: {self.tool}")
             return -1, None
-
-        if is_cuda:
-            selected_cpus = []
-        else:
-            selected_cpus = [v[0] for v in self.cpu_pairs if int(bench_args["cpumask"], 16) & (1 << v[0])]
 
         if self.stress and (stressed_cpus := [str(x) for x in range(len(self.cpu_pairs)) if x not in selected_cpus]):
             command = "\n".join([
@@ -234,19 +257,23 @@ class BenchHelper():
         cpu_freqs = [[cpu_freqs[idx], self.cpu_pairs[idx]] for idx in selected_cpus]
 
         result = {
+            "rw": rw,
             "qdepth": depth,
             "iosize": size,
+            "fio_size": self.fio_size,
             "ndevs": ndevs,
             "ncpus": ncpus,
             "nqueues": nqueues,
+            "device_bdf": bench_args["devices"][0] if bench_args["devices"] else None,
             "cpu_usage": cpu_usage,
             "cpu_freqs": cpu_freqs,
             "fixed_freq": self.cfm.fixed_freq,
             "cpu_governor": self.cfm.governor,
-            "thr_sib": 1 if self.use_thrsib else 0,
+            "cpu_control_supported": self.cfm.cpu_control_supported,
+            "thr_sib": self.use_thrsib,
             "smt": 1 if "SMT1" in suffix else 0,
             "turbo": 1 if "turbo1" in suffix else 0,
-            "stress": 1 if self.stress else 0,
+            "stress": self.stress,
             "tool": self.tool,
             "backend": self.backend,
             "iops": bench_result["total"]["iops"],
@@ -271,7 +298,6 @@ class BenchHelper():
             `(err, cpu_masks)` where a non-zero value for `err` describes that an error
             occured either while running `lscpu` or while parsing the output.
         """
-
         err, state = self.cijoe.run("lscpu -e")
         if err:
             log.error(f"Failed: lscpu -e")
@@ -284,7 +310,6 @@ class BenchHelper():
             return 1,
 
         cpu_pairs = [[int(v) for v in match.groupdict().values()] for match in matches]
-
         if use_thrsib:
             pairs = sorted(cpu_pairs, key=lambda p: p[1])
         else:
@@ -305,7 +330,30 @@ class BenchHelper():
 
         self.cpu_masks = cpu_masks
         self.cpu_pairs = cpu_pairs
+        return 0
 
+    def _prepare_fio_read_working_set(self, pci_addr: str, rw: str, backend: str) -> int:
+        key = (pci_addr, backend, self.fio_size, rw)
+        if key in self.fio_prefilled:
+            return 0
+
+        fio_job = create_fio_job(
+            pci_addr,
+            FIO_PREFILL_BS,
+            FIO_PREFILL_QD,
+            FIO_TIME_BASED_OFF,
+            "write",
+            backend,
+            self.fio_size,
+            time_based=False,
+        )
+
+        err, _ = self.cijoe.run(fio_xnvme_cmd("fio", fio_job))
+        if err:
+            log.error(f"Failed: fio prefill for backend({backend}) and device({pci_addr})")
+            return err
+
+        self.fio_prefilled.add(key)
         return 0
 
     def _parse_bench_results(self, table: str) -> Tuple[int, dict[str, list]]:
@@ -316,7 +364,6 @@ class BenchHelper():
         Returns `(err, result)`, where a non-zero value for `err` describes that the
         output did not match the expected format.
         """
-
         result = { "devices": [] }
         table_regex = None
 
@@ -326,6 +373,8 @@ class BenchHelper():
             table_regex = r"\s*(?P<name>.+?)\s*?:\s+(?P<iops>[0-9.]+)\s+(?P<mibs>[0-9.]+)\s+(?P<avg_lat>[0-9.]+)\s+(?P<min_lat>[0-9.]+)\s+(?P<max_lat>[0-9.]+)"
         elif self.tool in ["xnvmeperf", "xnvmeperf-cuda"]:
             table_regex = r"\s*(?P<name>\w+):?\s+(?P<cpus>[0-9,]+)?\s+(?P<iops>[0-9.]+)\s+(?P<mibs>[0-9.]+)\s+(?P<fails>[0-9.]+)"
+        elif self.tool == "fio_xnvme":
+            return self._parse_fio_results(table)
         else:
             log.error(f"Unkown tool: {self.tool}")
             return -1, None
@@ -341,6 +390,47 @@ class BenchHelper():
 
         return 0, result
 
+    def _parse_fio_results(self, output: str) -> Tuple[int, dict[str, list]]:
+        start = output.find("{")
+        end = output.rfind("}")
+        if start < 0 or end < 0 or end <= start:
+            log.error("Failed: could not find fio JSON output")
+            return 1, None
+
+        try:
+            payload = json.loads(output[start:end + 1])
+        except json.JSONDecodeError:
+            log.error("Failed: invalid fio JSON output")
+            return 1, None
+
+        jobs = payload.get("jobs", [])
+        if not jobs:
+            log.error("Failed: fio returned no jobs")
+            return 1, None
+
+        total_iops = 0.0
+        total_mibs = 0.0
+        devices = []
+        for job in jobs:
+            metrics = job.get("read", {})
+            if not float(metrics.get("iops", 0.0)):
+                write_metrics = job.get("write", {})
+                if float(write_metrics.get("iops", 0.0)):
+                    metrics = write_metrics
+            iops = float(metrics.get("iops", 0.0))
+            mibs = float(metrics.get("bw_bytes", 0.0)) / (1024 * 1024)
+            total_iops += iops
+            total_mibs += mibs
+            devices.append({"iops": iops, "mibs": mibs})
+
+        return 0, {
+            "devices": devices,
+            "total": {
+                "iops": total_iops,
+                "mibs": total_mibs,
+            },
+        }
+
     def _parse_time_output(self, output: str) -> Tuple[int, int]:
         """
         Find the CPU usage from /usr/bin/time in from the output of /usr/bin/time.
@@ -348,7 +438,6 @@ class BenchHelper():
         Returns `(err, cpu_usage)`, where a non-zero value for `err` describes that the
         output did not match the expected format.
         """
-
         time_regex = r"(?P<user>[0-9.]+)user (?P<system>[0-9.]+)system (?P<elapsed>[0-9.:]+)elapsed (?P<cpu>[0-9.]+)%CPU .*k"
         m = search(time_regex, output)
 

yonggilsong/aisio_fork

scripts/bench_helper.py
@@ -32,6 +32,7 @@ import logging as log
 
 from bdevperf import bdevperf_cmd, create_config as bdevperf_config
 from dcgm_helper import DcgmHelper
+from fio_xnvme import fio_xnvme_cmd, fio_xnvme_prefill_cmd
 from spdk_nvme_perf import spdk_nvme_perf_cmd
 from xnvmeperf import xnvmeperf_cmd, xnvmeperf_cuda_cmd
 
@@ -45,6 +46,7 @@ class BenchHelper():
             cfm: CpuFrequencyHelper,
             tool: str = "bdevperf",
             backend: str = "spdk",
+            fio_size: str = "16GiB",
     ):
         self.initialised = False
 
@@ -55,7 +57,7 @@ class BenchHelper():
         self.stress = False
         self.backend = backend
         self.tool = tool
-
+        self.fio_size = fio_size
         self.dcgm = DcgmHelper(cijoe) if backend == "upcie-cuda" else None
 
         self.use_thrsib = False
@@ -79,8 +81,11 @@ class BenchHelper():
                 self.bin = Path(spdk_path)  / "build" / "bin" / "spdk_nvme_perf"
         elif tool in ["xnvmeperf", "xnvmeperf-cuda"]:
             self.bin = "xnvmeperf"
+        elif tool == "fio_xnvme":
+            self.bin = "fio"
         else:
             log.error(f"Failed: Unknown tool({tool})")
+            return
 
         self.initialised = True
 
@@ -98,10 +103,9 @@ class BenchHelper():
             return err
 
         self.use_thrsib = use_thrsib
-
         return 0
 
-    def run_benchmark(self, depth: int, size: int, ndevs: int, ncpus: int, time: int, cpu_freq: float, suffix: str = "", nqueues: int = 1):
+    def run_benchmark(self, rw: str, depth: int, size: int, ndevs: int, ncpus: int, time: int, cpu_freq: float, suffix: str = "", nqueues: int = 1, prefill_only: int = 0):
         if not self.initialised:
             log.error("Failed: benchmarker not initialised correctly")
             return 1, None
@@ -114,10 +118,18 @@ class BenchHelper():
                 f"be_{self.backend}-tool_{self.tool}"
                 f"{suffix}.out"
             )
+        elif self.tool == "fio_xnvme":
+            filename = (
+                f"d{ndevs}-c{ncpus}-o{size}-f_{self.fio_size}-"
+                f"rw_{rw}-q{depth}-be_{self.backend}-tool_{self.tool}-"
+                f"thrsib{1 if self.use_thrsib else 0}-"
+                f"freq_{cpu_freq}-"
+                f"stress{1 if self.stress else 0}"
+                f"{suffix}.out"
+            )
         else:
             filename = (
-                f"d{ndevs}-c{ncpus}-o{size}-q{depth}-"
-                f"be_{self.backend}-tool_{self.tool}-"
+                f"d{ndevs}-c{ncpus}-o{size}-q{depth}-be_{self.backend}-tool_{self.tool}-"
                 f"thrsib{1 if self.use_thrsib else 0}-"
                 f"freq_{cpu_freq}-"
                 f"stress{1 if self.stress else 0}"
@@ -131,17 +143,35 @@ class BenchHelper():
                 return 0, result
 
         bench_args = {
-            "iopattern": "randread",
+            "iopattern": rw,
             "qdepth": depth,
             "iosize": size,
             "runtime": time,
             "devices": [d["pci_addr"] for d in self.devices[0:ndevs]],
         }
 
+        if self.tool == "fio_xnvme":
+            bench_args["backend"] = self.backend
+            bench_args["rw"] = rw
+            bench_args["fio_size"] = self.fio_size
+
         if not is_cuda:
             bench_args["cpumask"] = self.cpu_masks[ncpus]
 
-        command = f"/usr/bin/time "
+        if self.tool == "fio_xnvme" and ndevs != 1:
+            log.error("Failed: fio_xnvme currently supports exactly 1 device per benchmark point")
+            return 1, None
+
+        if is_cuda:
+            selected_cpus = []
+            command = f"/usr/bin/time "
+        else:
+            selected_cpus = [v[0] for v in self.cpu_pairs if int(bench_args["cpumask"], 16) & (1 << v[0])]
+            command = f"/usr/bin/time "
+
+        if self.tool == "fio_xnvme":
+            cpu_list = ",".join(str(cpu) for cpu in selected_cpus)
+            command = f"taskset -c {cpu_list} {command}"
 
         if self.tool == "bdevperf":
             config_local_path = self.configs_path / f"d{ndevs}.json"
@@ -150,10 +180,8 @@ class BenchHelper():
 
             bench_args["config_path"] = self.remote_config
             command += bdevperf_cmd(self.bin, bench_args)
-
         elif self.tool == "spdk_nvme_perf":
             command += spdk_nvme_perf_cmd(self.bin, bench_args)
-
         elif self.tool == "xnvmeperf":
             bench_args["backend"] = self.backend
             command += xnvmeperf_cmd(self.bin, bench_args)
@@ -163,14 +191,14 @@ class BenchHelper():
             bench_args["nqueues"] = nqueues
             command += xnvmeperf_cuda_cmd(self.bin, bench_args)
 
+        elif self.tool == "fio_xnvme":
+            if prefill_only:
+                command += fio_xnvme_prefill_cmd(self.bin, bench_args)
+            else:
+                command += fio_xnvme_cmd(self.bin, bench_args)
         else:
             log.error(f"Unknown tool: {self.tool}")
             return -1, None
-
-        if is_cuda:
-            selected_cpus = []
-        else:
-            selected_cpus = [v[0] for v in self.cpu_pairs if int(bench_args["cpumask"], 16) & (1 << v[0])]
 
         if self.stress and (stressed_cpus := [str(x) for x in range(len(self.cpu_pairs)) if x not in selected_cpus]):
             command = "\n".join([
@@ -234,19 +262,23 @@ class BenchHelper():
         cpu_freqs = [[cpu_freqs[idx], self.cpu_pairs[idx]] for idx in selected_cpus]
 
         result = {
+            "rw": rw,
             "qdepth": depth,
             "iosize": size,
+            "fio_size": self.fio_size,
             "ndevs": ndevs,
             "ncpus": ncpus,
             "nqueues": nqueues,
+            "device_bdf": bench_args["devices"][0] if bench_args["devices"] else None,
             "cpu_usage": cpu_usage,
             "cpu_freqs": cpu_freqs,
             "fixed_freq": self.cfm.fixed_freq,
             "cpu_governor": self.cfm.governor,
-            "thr_sib": 1 if self.use_thrsib else 0,
+            "cpu_control_supported": 1 if self.cfm.cpu_control_supported else 0,
+            "thr_sib": self.use_thrsib,
             "smt": 1 if "SMT1" in suffix else 0,
             "turbo": 1 if "turbo1" in suffix else 0,
-            "stress": 1 if self.stress else 0,
+            "stress": self.stress,
             "tool": self.tool,
             "backend": self.backend,
             "iops": bench_result["total"]["iops"],
@@ -271,7 +303,6 @@ class BenchHelper():
             `(err, cpu_masks)` where a non-zero value for `err` describes that an error
             occured either while running `lscpu` or while parsing the output.
         """
-
         err, state = self.cijoe.run("lscpu -e")
         if err:
             log.error(f"Failed: lscpu -e")
@@ -284,7 +315,6 @@ class BenchHelper():
             return 1,
 
         cpu_pairs = [[int(v) for v in match.groupdict().values()] for match in matches]
-
         if use_thrsib:
             pairs = sorted(cpu_pairs, key=lambda p: p[1])
         else:
@@ -305,7 +335,6 @@ class BenchHelper():
 
         self.cpu_masks = cpu_masks
         self.cpu_pairs = cpu_pairs
-
         return 0
 
     def _parse_bench_results(self, table: str) -> Tuple[int, dict[str, list]]:
@@ -316,7 +345,6 @@ class BenchHelper():
         Returns `(err, result)`, where a non-zero value for `err` describes that the
         output did not match the expected format.
         """
-
         result = { "devices": [] }
         table_regex = None
 
@@ -326,6 +354,8 @@ class BenchHelper():
             table_regex = r"\s*(?P<name>.+?)\s*?:\s+(?P<iops>[0-9.]+)\s+(?P<mibs>[0-9.]+)\s+(?P<avg_lat>[0-9.]+)\s+(?P<min_lat>[0-9.]+)\s+(?P<max_lat>[0-9.]+)"
         elif self.tool in ["xnvmeperf", "xnvmeperf-cuda"]:
             table_regex = r"\s*(?P<name>\w+):?\s+(?P<cpus>[0-9,]+)?\s+(?P<iops>[0-9.]+)\s+(?P<mibs>[0-9.]+)\s+(?P<fails>[0-9.]+)"
+        elif self.tool == "fio_xnvme":
+            return self._parse_fio_results(table)
         else:
             log.error(f"Unkown tool: {self.tool}")
             return -1, None
@@ -341,6 +371,47 @@ class BenchHelper():
 
         return 0, result
 
+    def _parse_fio_results(self, output: str) -> Tuple[int, dict[str, list]]:
+        start = output.find("{")
+        end = output.rfind("}")
+        if start < 0 or end < 0 or end <= start:
+            log.error("Failed: could not find fio JSON output")
+            return 1, None
+
+        try:
+            payload = json.loads(output[start:end + 1])
+        except json.JSONDecodeError:
+            log.error("Failed: invalid fio JSON output")
+            return 1, None
+
+        jobs = payload.get("jobs", [])
+        if not jobs:
+            log.error("Failed: fio returned no jobs")
+            return 1, None
+
+        total_iops = 0.0
+        total_mibs = 0.0
+        devices = []
+        for job in jobs:
+            metrics = job.get("read", {})
+            if not float(metrics.get("iops", 0.0)):
+                write_metrics = job.get("write", {})
+                if float(write_metrics.get("iops", 0.0)):
+                    metrics = write_metrics
+            iops = float(metrics.get("iops", 0.0))
+            mibs = float(metrics.get("bw_bytes", 0.0)) / (1024 * 1024)
+            total_iops += iops
+            total_mibs += mibs
+            devices.append({"iops": iops, "mibs": mibs})
+
+        return 0, {
+            "devices": devices,
+            "total": {
+                "iops": total_iops,
+                "mibs": total_mibs,
+            },
+        }
+
     def _parse_time_output(self, output: str) -> Tuple[int, int]:
         """
         Find the CPU usage from /usr/bin/time in from the output of /usr/bin/time.
@@ -348,7 +419,6 @@ class BenchHelper():
         Returns `(err, cpu_usage)`, where a non-zero value for `err` describes that the
         output did not match the expected format.
         """
-
         time_regex = r"(?P<user>[0-9.]+)user (?P<system>[0-9.]+)system (?P<elapsed>[0-9.:]+)elapsed (?P<cpu>[0-9.]+)%CPU .*k"
         m = search(time_regex, output)
 